4285d64cf08b420a2c0675240cc6b65ab871d002
manujainz <manujainpixels@gmail.com>
2023-10-29 03:35:06 +0530

    Whitelisted policies in Policy Repository

--- src/main/kotlin/com/manujainz/kotlyze/policies/base/factory/PolicyRepository.kt
+++ src/main/kotlin/com/manujainz/kotlyze/policies/base/factory/PolicyRepository.kt
@@ -1,20 +1,31 @@
 package com.manujainz.kotlyze.policies.base.factory
 
 import com.manujainz.kotlyze.config.ConfigLoader
-import com.manujainz.kotlyze.policies.complexity.ComplexConditionChain
-import com.manujainz.kotlyze.policies.codestyle.MaxCharacterPerLine
-import com.manujainz.kotlyze.policies.codestyle.MethodExplosionInClass
-import com.manujainz.kotlyze.policies.perf.CoroutineGlobalScopeUsage
+import com.manujainz.kotlyze.policies.codestyle.*
+import com.manujainz.kotlyze.policies.complexity.*
+import com.manujainz.kotlyze.policies.perf.*
 import com.manujainz.kotlyze.reporting.core.ReportEngine
 
 class PolicyRepository(configLoader: ConfigLoader, reportEngine: ReportEngine) {
 
     val policies by lazy {
-        listOf(
+        setOf(
+            MagicNumbersPolicy(configLoader, reportEngine),
             MaxCharacterPerLine(configLoader, reportEngine),
+            MethodExplosionInClass(configLoader, reportEngine),
+            MultipleClassesPerFile(configLoader, reportEngine),
+            TrailingWhitespacePolicy(configLoader, reportEngine),
+            UnusedImportsPolicy(configLoader, reportEngine),
             ComplexConditionChain(configLoader, reportEngine),
+            ExcessiveReturnStatements(configLoader, reportEngine),
+            HighCyclomaticComplexity(configLoader, reportEngine),
+            NestedDepth(configLoader, reportEngine),
+            RecursiveFunctionUsage(configLoader, reportEngine),
+            BlockingCallInCoroutine(configLoader, reportEngine),
             CoroutineGlobalScopeUsage(configLoader, reportEngine),
-            MethodExplosionInClass(configLoader, reportEngine)
+            InefficientCollectionOperation(configLoader, reportEngine),
+            InefficientOperationsWhileLooping(configLoader, reportEngine),
+            LargeBitmapLoading(configLoader, reportEngine),
         )
     }
 }
